684cdfaa6145c7ac7574c060d74c8f1d8eaad691
oskarcokl <oskar.cokl@gmail.com>
2024-11-09 22:30:43 +0100

    feat: Add basic AuthContext.

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface AuthContextProvider {
+    token: string | null
+    setToken: (a: string) => void
+}
+
+
+const AuthContext = createContext<AuthContextProvider | null>(null);
+
+const AuthProvider = ({children}: {children: ReactNode}) => {
+    const [token, setToken_] = useState(localStorage.getItem("token"));
+
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        localStorage.setItem("token", token);
+    } else {
+        delete axios.defaults.headers.common["Authorization"];
+        localStorage.removeItem("token");
+    }
+
+    const setToken = (newToken: string) => {
+        setToken_(newToken);
+    }
+
+    return (
+        <AuthContext.Provider value={{token, setToken}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export default AuthProvider;
